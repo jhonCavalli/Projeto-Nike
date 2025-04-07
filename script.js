27766819ad1ba11f6514a2c22419f138c4bb8258
jhon Cavalli <jhoncavalli@hotmail.com>
2025-04-06 23:41:47 -0300

    adiciona comentários explicativos e reorganiza a definição da função mudarVisual no script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function mudarVisual(){
+let body = document.querySelector("body");
+// document = html
+// querySelector = pegador / selecionador de elementos
 
+function mudarVisual(){
+    body.style.background = "red";
 }
 
